80055d57dee291d54069a6c560b6e13d4f12b827
viknesh1234 <vikneshr07@gmail.com>
2025-04-16 15:17:52 +0530

    new code

---
+++ stringbuffer.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class stringbuffer {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+    
+        StringBuffer buffer = new StringBuffer("Hello");
+        for(int i=0; i<1000;i++){
+            buffer.append("World");
+        }
+        System.out.println("Time taken to append 1000 times: " + (System.currentTimeMillis() - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        StringBuilder builder = new StringBuilder("Hello");
+        for(int i=0;i<1000;i++){
+            builder.append("world");
+        }
+        System.out.println("Time taken to append 1000 times: " + (System.currentTimeMillis() - startTime) + "ms");
+        }
+}
