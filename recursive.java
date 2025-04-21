5c58fbc82142b44893b738105e2fb1b40d1d4c57
viknesh1234 <vikneshr07@gmail.com>
2025-04-21 14:32:02 +0530

    new code

---
+++ recursive.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class recursive {
+    // public static int fun(int n) {
+    //     if(n==0){
+    //         return 0;
+    //     }
+    //   return fun(n/10)+1;
+        
+    // }
+    // public static void main(String[] args) {
+    //     int sum = fun(012345);
+    //     System.out.println(sum);
+    // }
+    // public static int sumofDigit(int n, int rev) {
+    //     if(n==0){
+    //         return rev;
+    //     }
+    //     rev = rev*10+n%10;
+    //     return sumofDigit(n/10)+n%10;
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     System.out.println(sumofDigit(n));
+   // }
+
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Number : ");
+    int n =sc.nextInt();
+    int mask = Integer.MIN_VALUE;
+    String s = "";
+    while(mask!=0){
+        if((n&mask) ==0){
+            s=s+"0";
+        }
+        else{
+            s = s+"1";
+        }
+        mask = mask>>>1;
+    }
+    System.out.println("Binary value : ");
+    System.out.println(s);
+
+   }
+}
